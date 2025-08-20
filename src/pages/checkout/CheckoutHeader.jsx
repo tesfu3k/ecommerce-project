ff6aed72c15c6cdbb11c1d33f0274a7cb32aefd9
tesfu3k <tesfaye4r@gmail.com>
2025-08-20 22:37:22 +0300

    Rename header.css to Header.css

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -1,20 +1,21 @@
+import { Link } from "react-router";
 import "./CheckoutHeader.css";
 export function CheckoutHeader() {
   return (
     <div className="checkout-header">
       <div className="header-content">
         <div className="checkout-header-left-section">
-          <a href="/">
+          <Link to="/">
             <img className="logo" src="images/logo.png" />
             <img className="mobile-logo" src="images/mobile-logo.png" />
-          </a>
+          </Link>
         </div>
 
         <div className="checkout-header-middle-section">
           Checkout (
-          <a className="return-to-home-link" href="/">
+          <Link className="return-to-home-link" to="/">
             3 items
-          </a>
+          </Link>
           )
         </div>
 
