71ca3d51e16cecb7425bc8456e82c93c1d8c40e5
tesfu3k <tesfaye4r@gmail.com>
2025-08-21 22:15:43 +0300

    Fetch data from the backend

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -2,6 +2,20 @@ import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 export function HomePage() {
+  // fetch("http://localhost:3000/api/products").then((response) => {
+  //   response.json().then((data) => {
+  //     console.log(data);
+  //   });
+  // });
+
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
