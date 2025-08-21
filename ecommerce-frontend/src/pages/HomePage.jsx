1b5baf49ef9f00501d2722c3869bfc4d86d41521
tesfu3k <tesfaye4r@gmail.com>
2025-08-21 22:24:25 +0300

    Use axios to fetch data

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
@@ -8,13 +9,9 @@ export function HomePage() {
   //   });
   // });
 
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
