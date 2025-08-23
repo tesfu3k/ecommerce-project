d67f84732efdd9c29ba5e20859c96bce653a78bd
tesfu3k <tesfaye4r@gmail.com>
2025-08-24 00:41:52 +0300

    Convert home and checkout to async await

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -12,10 +12,18 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/cart-items?expand=product").then((response) => {
+    const fetchAppData = async () => {
+      const response = await axios.get("/api/cart-items?expand=product");
       setCart(response.data);
-    });
+    };
+    fetchAppData();
   }, []);
+
+  // useEffect(() => {
+  //   axios.get("/api/cart-items?expand=product").then((response) => {
+  //     setCart(response.data);
+  //   });
+  // }, []);
 
   return (
     <Routes>
